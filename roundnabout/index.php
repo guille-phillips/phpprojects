944a0ec4ff2ee0d3f1f82d026dac0210fb91bea4
Guillermo Phillips <guillermo.phillips@chromasports.com>
2015-09-09 15:58:31 +0100

    Further bubble styling

--- roundnabout/index.php
+++ roundnabout/index.php
@@ -32,6 +32,8 @@
 			var marker_resource = 'resources/pin-144ppi.png';
 			var home_marker_resource = 'resources/home-marker.png';
 			
+			var previous_marker_id = undefined;
+
 			function LocationSuccess(pos) {
 				var crd = pos.coords;
 				initial_latlong = [crd.latitude, crd.longitude];
@@ -132,7 +134,8 @@
 				var marker_index = 1;
 				for (var index in places) {
 					var place = places[index];
-					AddMarker(place.id,marker_index,place.latitude,place.longitude,marker_resource,MarkerClicked);
+
+					AddMarker(place, place.id,marker_index,place.latitude,place.longitude,marker_resource,MarkerClicked);
 					
 					// Place List Item
 					var div = document.createElement('div');
@@ -151,7 +154,11 @@
 			
 			function CreateInfoBox(place) {
 				var html_array = [];
+
+				html_array.push(Tag('img','',{class:'square'}));
+
 				html_array.push(Tag('h1',place.name));
+				
 				if (place.category.join) {
 					html_array.push( place.category.map(function(content){return Tag('div',content,{class:'category_item'});}).join('') );
 				}
@@ -161,15 +168,15 @@
 				}
 				
 				if (place.telephone.join) {
-					html_array.push( Tag('div',place.telephone.join(', '),{class:'address'}) );
+					html_array.push( Tag('div',place.telephone.join(', '),{class:'telephone'}) );
 				}
 				html_array.push('Opening Times:');
 				if (place.opening_times.join) {
-					html_array.push( Tag('div',place.opening_times.join(', '),{class:'address'}) );
+					html_array.push( Tag('div',place.opening_times.join(', '),{class:'opening_times'}) );
 				}
 				html_array.push('Entry Rates:');
 				if (place.entry_rates.join) {
-					html_array.push( Tag('div',place.entry_rates.join(', '),{class:'address'}) );
+					html_array.push( Tag('div',place.entry_rates.join(', '),{class:'entry_rates'}) );
 				}				
 				
 				return html_array.join('');
@@ -177,10 +184,21 @@
 
 			function MarkerClicked(marker) {
 				//alert('marker clicked:'+marker.dataset.marker_id);
-				document.getElementById("bubble"+marker.dataset.marker_id).style.display="inherit";
+				if (previous_marker_id!==undefined) {
+					document.getElementById("bubble"+previous_marker_id).style.display="none";
+					if (previous_marker_id !== marker.dataset.marker_id) {
+						document.getElementById("bubble"+marker.dataset.marker_id).style.display="inherit";
+					} else {
+						previous_marker_id = undefined;
+					}
+				} else {
+					document.getElementById("bubble"+marker.dataset.marker_id).style.display="inherit";
+				}
+				
+				previous_marker_id = marker.dataset.marker_id;
 			}
 			
-			function AddMarker(id,name,lat,lon,resource,callback) {
+			function AddMarker(place, id,name,lat,lon,resource,callback) {
 				console.log('AddMarker:'+id);
 				
 				var map_box = document.getElementById('google_map');
@@ -196,8 +214,9 @@
 					}
 				);
 
+				var info_html = CreateInfoBox(place);
 
-				var bubble_html = "<div id='bubble"+id+"' class='marker-bubble-left'>"+'xyz'+"</div>";
+				var bubble_html = "<div id='bubble"+id+"' class='marker-bubble-left'>"+info_html+"</div>";
 				var overlay = new CustomMarker(
 					new google.maps.LatLng(lat, lon), 
 					map,
